6025c422585e3e9d986e7a586a83158a6b3e3a70
Cooper Runyan <73623839+cooperrunyan@users.noreply.github.com>
2022-02-19 11:53:16 -0700

    Revert "Prettier config"

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,6 +1,8 @@
-export const mongo = {
-  mechanism: 'SCRAM-SHA-1',
-  database: 'stocked',
-  tls: true,
-  servers: ['cluster-shard-00-02.nwc8t.mongodb.net', 'cluster-shard-00-01.nwc8t.mongodb.net', 'cluster-shard-00-00.nwc8t.mongodb.net'],
-} as const;
+export default {
+  mongo: {
+    mechanism: 'SCRAM-SHA-1',
+    database: 'stocked',
+    tls: 'true',
+    servers: ['cluster-shard-00-02.nwc8t.mongodb.net', 'cluster-shard-00-01.nwc8t.mongodb.net', 'cluster-shard-00-00.nwc8t.mongodb.net'],
+  },
+};
