ff07b49b5e435a6ca097b7ce8d51be5c101ca6a3
cooperrunyan <cooperrunyanspam@gmail.com>
2022-02-19 11:35:57 -0700

    Fixed a config type error (added "as const")

--- src/server/config.ts
+++ src/server/config.ts
@@ -5,4 +5,4 @@ export default {
     tls: 'true',
     servers: ['cluster-shard-00-02.nwc8t.mongodb.net', 'cluster-shard-00-01.nwc8t.mongodb.net', 'cluster-shard-00-00.nwc8t.mongodb.net'],
   },
-};
+} as const;
