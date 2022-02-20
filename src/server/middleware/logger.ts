1745ac62d0e6084ed075eeb79704d88fc6f5b09d
Cooper Runyan <cooperrunyanspam@gmail.com>
2022-02-19 18:19:53 -0700

    Touched up middleware

--- src/server/middleware/logger.ts
+++ src/server/middleware/logger.ts
@@ -1,6 +1,12 @@
 import { oak } from '../../deps.ts';
+import { jwt } from '../encryption/index.ts';
 
 export async function logger(ctx: oak.Context, next: () => Promise<unknown>) {
+  const startTime = Date.now();
   await next();
-  console.log(`${ctx.request.method} ${ctx.request.url.pathname} ${ctx.response.status}`);
+  console.log(
+    `${ctx.request.method} ${ctx.response.status} ${Date.now() - startTime}ms ${
+      ((await jwt.validate((await ctx.cookies.get('jwt')) as string)) as any)?.payload?.iss || 'NOT_LOGGED_IN'
+    } ${ctx.request.url.pathname}`,
+  );
 }
